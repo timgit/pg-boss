01ffb4b8117e3bcdc8d11681663a822e2c12e296
Tim Jones <timgit@users.noreply.github.com>
2016-03-16 00:14:32 -0500

    minor integration test updates - switched back to src from lib with --use_strict because I like webstorm debugging so much

--- test/delayajob.js
+++ test/delayajob.js
@@ -1,27 +1,25 @@
-const PgBoss = require('../lib/index');
+const PgBoss = require('../src/index');
 const config = require('./config.json');
 
 var boss = new PgBoss(config);
 
 boss.on('error', error => console.error(error));
-//boss.on('job', job => console.log(`${job.name} job found - ${job.id}`));
 boss.on('ready', init);
 
 function init() {
 
-    var start = new Date();
-
-    boss.registerJob('wait5seconds', {teamSize: 1}, (job, done) => {
+    boss.registerJob('wait5seconds', null, (job, done) => {
+        var start = new Date(job.data.submitted);
         var end = new Date();
 
         console.log(`${end-start} got job ${job.id} payload: ${job.data.message}`);
 
-        done().then(() => process.exit(0));
+        done().then(() => process.exit());
     });
 
-
-
-    boss.submitJob('wait5seconds', {message: 'hold your horses'}, {startIn: 5})
-        .then(jobId => console.log(`job id ${jobId} submitted`));
+    boss.submitJob('wait5seconds', {message: 'hold your horses', submitted: Date.now()}, {startIn: 5})
+        .then(jobId => {
+            console.log(`job id ${jobId} submitted`);
+        });
 
 }
--- test/hundredjobs.js
+++ test/hundredjobs.js
@@ -1,22 +1,28 @@
-const PgBoss = require('../lib/index');
+const PgBoss = require('../src/index');
 const config = require('./config.json');
 
 var boss = new PgBoss(config);
 
 boss.on('error', error => console.error(error));
-boss.on('job', job => console.log(`${job.name} job found - ${job.id}`));
 boss.on('ready', init);
 
 function init() {
 
-    boss.registerJob('log', {teamSize: 20}, (job, done) => {
+    var receivedCount = 0;
+
+    boss.registerJob('oneinahundred', {teamSize: 20}, (job, done) => {
         console.log(`got job ${job.id} payload: ${job.data.message}`);
-        done();
+        done().then(() => {
+            receivedCount++;
+
+            if(receivedCount === 100)
+                process.exit();
+        });
     });
 
     for(var x=0; x<100; x++){
 
-        boss.submitJob('log', {message: x + ' requested'})
+        boss.submitJob('oneinahundred', {message: x + ' requested'})
             .then(jobId => console.log(`job id ${jobId} submitted`));
 
     }
