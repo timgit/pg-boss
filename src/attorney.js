e15bfefa77cf735b775e1d785c154c51ce1d5ba1
Tim Jones <timgit@users.noreply.github.com>
2016-03-18 02:25:25 -0500

    updated config validation prompts

--- src/attorney.js
+++ src/attorney.js
@@ -2,26 +2,26 @@ const assert = require('assert');
 
 module.exports = {
     checkConfig: checkConfig
-}
+};
 
 function checkConfig(config) {
     assert(config && (typeof config == 'object' || typeof config == 'string'),
-        'string or config object is required to connect to postgres');
+        'configuration assert: string or config object is required to connect to postgres');
 
     if(typeof config == 'string')
         config = {connectionString: config};
     else {
         assert(config.database && config.user && 'password' in config,
-            'expected configuration object to have enough information to connect to PostgreSQL');
+            'configuration assert: not enough database settings to connect to PostgreSQL');
 
         config.host = config.host || 'localhost';
         config.port = config.port || 5432;
     }
 
     if(config.schema){
-        assert(typeof config.schema == 'string', 'schema must be a string');
-        assert(config.schema.length < 20, 'schema should be between 1 and 20 characters');
-        assert(!/\W/.test(config.schema), `invalid: ${config.schema} cannot be used as a schema. Only have alphanumeric characters and underscores are allowed`);
+        assert(typeof config.schema == 'string', 'configuration assert: schema must be a string');
+        assert(config.schema.length < 20, 'configuration assert: schema should be between 1 and 20 characters');
+        assert(!/\W/.test(config.schema), `configuration assert: ${config.schema} cannot be used as a schema. Only alphanumeric characters and underscores are allowed`);
     }
 
     if(config.newJobCheckIntervalSeconds)
@@ -37,7 +37,7 @@ function checkConfig(config) {
         assert(typeof config.archiveCompletedJobsEvery == 'string', 'configuration assert: archiveCompletedJobsEvery should be a readable PostgreSQL interval such as "1 day"');
 
     if(config.uuid)
-        assert(config.uuid == 'v1' || config.uuid == 'v4', 'uuid option only supports v1 or v4');
+        assert(config.uuid == 'v1' || config.uuid == 'v4', 'configuration assert: uuid option only supports v1 or v4');
 
 
     config.uuid = config.uuid || 'v1';
