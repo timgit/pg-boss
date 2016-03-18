84b929b53f74bb217137e6a955e8cdf49886c4fc
Tim Jones <timgit@users.noreply.github.com>
2016-03-18 02:10:36 -0500

    created a test helper service

---
+++ test/testService.js
@@ -0,0 +1,16 @@
+const Db = require('../src/db');
+const config = require('./config.json');
+
+module.exports = {
+  init: init
+};
+
+function init() {
+    const schema = config.schema || 'pgboss';
+    const emptyJobsCommand = `truncate table ${schema}.job`;
+
+    var db = new Db(config);
+
+    return db.executeSql(emptyJobsCommand)
+        .catch(error => console.error(error));
+}
