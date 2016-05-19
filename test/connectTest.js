de0d078f3bdddd40a05c64870cc5bc36667baf23
Tim Jones <me.timjones@gmail.com>
2016-05-19 01:48:08 -0500

    added happy path connect test

--- test/connectTest.js
+++ test/connectTest.js
@@ -1,12 +1,11 @@
 var assert = require('chai').assert;
-var PgBoss = require('../src/index');
 var helper = require('./testHelper');
 
 describe('connect', function() {
 
     var boss;
 
-    before(function(finished){
+    beforeEach(function(finished){
         helper.start()
             .then(dabauce => {
                 boss = dabauce;
@@ -15,7 +14,6 @@ describe('connect', function() {
     });
 
     it('should fail if connecting to an older schema version', function (finished) {
-
         helper.getDb().executeSql(`UPDATE ${helper.config.schema}.version SET VERSION = '0.0.0'`)
             .then(() => {
                 boss.on('error', function (error) {
@@ -25,6 +23,15 @@ describe('connect', function() {
 
                 boss.connect();
             });
+    });
+
+    it('should succeed if already started', function (finished) {
+        boss.on('ready', () => {
+            assert(true);
+            finished();
+        });
+
+        boss.connect();
     });
 
 });
