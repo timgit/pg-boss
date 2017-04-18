b3b5c60c7b2bfeec880016ded8a1cbe1805e86f7
Tim Jones <me.timjones@gmail.com>
2017-04-17 23:54:45 -0500

    integrating db tear down into stop and disconnect api and affected tests

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const Db = require('./db');
 
 const notReadyErrorMessage = `boss ain't ready.  Use start() or connect() to get started.`;
 const startInProgressErrorMessage = 'boss is starting up. Please wait for the previous start() to finish.';
+const notStartedErrorMessage = `boss ain't started.  Use start().`;
 
 class PgBoss extends EventEmitter {
   static getConstructionPlans(schema) {
@@ -24,24 +25,25 @@ class PgBoss extends EventEmitter {
 
     super();
 
-    this.config = config;
-
     const db = new Db(config);
 
     promoteEvent.call(this, db, 'error');
 
-    this.contractor = new Contractor(db, config);
-
     const boss = new Boss(db, config);
     boss.promotedEvents.forEach(event => promoteEvent.call(this, boss, event));
-    this.boss = boss;
 
     const manager = new Manager(db, config);
     manager.promotedEvents.forEach(event => promoteEvent.call(this, manager, event));
-    this.manager = manager;
 
     ['fetch','complete','cancel','fail','publish','subscribe','unsubscribe','onComplete','onExpire']
       .forEach(func => promoteApi.call(this, manager, func));
+
+    this.config = config;
+    this.db = db;
+    this.boss = boss;
+    this.contractor = new Contractor(db, config);
+    this.manager = manager;
+
 
     function promoteApi(obj, func){
       this[func] = (...args) => {
@@ -59,50 +61,59 @@ class PgBoss extends EventEmitter {
   init() {
     if(this.isReady) return Promise.resolve(this);
 
-    return this.boss.supervise()
-      .then(() => this.manager.monitor())
+    return Promise.join(
+      this.boss.supervise(),
+      this.manager.monitor()
+    ).then(() => {
+        this.isReady = true;
+        this.isStarted = true;
+        return this;
+    });
+  }
+
+  start(...args) {
+    if(this.isStarting)
+      return Promise.reject(startInProgressErrorMessage);
+
+    this.isStarting = true;
+
+    let check = this.isStarted
+      ? Promise.resolve(true)
+      : this.contractor.start.apply(this.contractor, args);
+
+    return check.then(() => {
+        this.isStarting = false;
+        return this.init();
+    });
+  }
+
+  stop() {
+    if(!this.isStarted) return Promise.reject(notStartedErrorMessage);
+
+    return Promise.join(
+        this.manager.stop(),
+        this.boss.stop()
+      )
+      .then(() => this.db.close())
+      .then(() => {
+        this.isReady = false;
+        this.isStarted = false;
+      });
+  }
+
+  connect(...args) {
+    return this.contractor.connect.apply(this.contractor, args)
       .then(() => {
         this.isReady = true;
         return this;
       });
   }
 
-  start(...args) {
-    let self = this;
-
-    if(this.isStarting)
-      return Promise.reject(startInProgressErrorMessage);
-
-    this.isStarting = true;
-
-    return this.contractor.start.apply(this.contractor, args)
-      .then(() => {
-        self.isStarting = false;
-        return self.init();
-      });
-  }
-
-  stop() {
-    return Promise.all([
-      this.disconnect(),
-      this.manager.stop(),
-      this.boss.stop()
-    ]);
-  }
-
-  connect(...args) {
-    let self = this;
-
-    return this.contractor.connect.apply(this.contractor, args)
-      .then(() => {
-        self.isReady = true;
-        return self;
-      });
-  }
-
   disconnect(...args) {
     if(!this.isReady) return Promise.reject(notReadyErrorMessage);
+
     return this.manager.close.apply(this.manager, args)
+      .then(() => this.db.close())
       .then(() => this.isReady = false);
   }
 
--- test/throttleTest.js
+++ test/throttleTest.js
@@ -3,124 +3,136 @@ const helper = require('./testHelper');
 
 describe('throttle', function() {
 
-    let boss;
+  let boss;
 
-    before(function(finished){
-        helper.start()
-            .then(dabauce => {
-                boss = dabauce;
-                finished();
-            });
+  before(function(finished){
+    helper.start()
+      .then(dabauce => {
+        boss = dabauce;
+        finished();
+      });
+  });
+
+  after(function(finished){
+    boss.stop().then(() => finished());
+  });
+
+  it('should only create 1 job for interval with a delay', function(finished){
+
+    const jobName = 'delayThrottle'
+    const singletonSeconds = 4;
+    const startIn = '2 seconds';
+
+    const jobCount = 1;
+
+    const publishInterval = 500;
+    const assertTimeout = 4000;
+
+    this.timeout(assertTimeout + 1000);
+
+    let publishCount = 0;
+    let subscribeCount = 0;
+
+    boss.subscribe(jobName, function(job, done) {
+      done().then(function() { subscribeCount++; });
     });
 
-    after(function(finished){
-        boss.stop().then(() => finished());
+    let intervalId;
+    let shuttingDown = false;
+
+    setTimeout(function() {
+      console.log('published ' + publishCount + ' jobs in '  + assertTimeout/1000 + ' seconds but received ' + subscribeCount + ' jobs');
+      assert.isAtMost(subscribeCount, jobCount + 1);
+
+      shuttingDown = true;
+      clearInterval(intervalId);
+      finished();
+
+    }, assertTimeout);
+
+
+    intervalId = setInterval(function() {
+      if(shuttingDown) return;
+      boss.publish(jobName, null, {startIn, singletonSeconds})
+        .then(function() { publishCount++; });
+    }, publishInterval);
+  });
+
+  it('should process at most 1 job per second', function (finished) {
+
+    const singletonSeconds = 1;
+    const jobCount = 3;
+    const publishInterval = 100;
+    const assertTimeout = jobCount * 1000;
+
+    // add an extra second to test timeout
+    this.timeout((jobCount + 1) * 1000);
+
+    let publishCount = 0;
+    let subscribeCount = 0;
+
+    boss.subscribe('expensive', function(job, done) {
+      done().then(function() { subscribeCount++; });
     });
 
-    it('should only create 1 job for interval with a delay', function(finished){
+    let intervalId;
+    let shuttingDown = false;
 
-        const jobName = 'delayThrottle'
-        const singletonSeconds = 4;
-        const startIn = '2 seconds';
+    setTimeout(function() {
+      console.log('published ' + publishCount + ' jobs in '  + assertTimeout/1000 + ' seconds but received ' + subscribeCount + ' jobs');
+      assert.isAtMost(subscribeCount, jobCount + 1);
 
-        const jobCount = 1;
+      shuttingDown = true;
+      clearInterval(intervalId);
+      finished();
 
-        const publishInterval = 500;
-        const assertTimeout = 4000;
-
-        this.timeout(assertTimeout + 1000);
-
-        let publishCount = 0;
-        let subscribeCount = 0;
-
-        boss.subscribe(jobName, function(job, done) {
-            done().then(function() { subscribeCount++; });
-        });
-
-        setTimeout(function() {
-            console.log('published ' + publishCount + ' jobs in '  + assertTimeout/1000 + ' seconds but received ' + subscribeCount + ' jobs');
-            assert.isAtMost(subscribeCount, jobCount + 1);
-
-            finished();
-
-        }, assertTimeout);
+    }, assertTimeout);
 
 
-        setInterval(function() {
-            boss.publish(jobName, null, {startIn, singletonSeconds})
-                .then(function() { publishCount++; });
-        }, publishInterval);
-    });
+    intervalId = setInterval(function() {
+      if(shuttingDown) return;
+      boss.publish('expensive', null, {singletonSeconds: singletonSeconds})
+        .then(function() { publishCount++; });
+    }, publishInterval);
 
-    it('should process at most 1 job per second', function (finished) {
-
-        const singletonSeconds = 1;
-        const jobCount = 3;
-        const publishInterval = 100;
-        const assertTimeout = jobCount * 1000;
-
-        // add an extra second to test timeout
-        this.timeout((jobCount + 1) * 1000);
-
-        let publishCount = 0;
-        let subscribeCount = 0;
-
-        boss.subscribe('expensive', function(job, done) {
-            done().then(function() { subscribeCount++; });
-        });
-
-        setTimeout(function() {
-            console.log('published ' + publishCount + ' jobs in '  + assertTimeout/1000 + ' seconds but received ' + subscribeCount + ' jobs');
-            assert.isAtMost(subscribeCount, jobCount + 1);
-
-            finished();
-
-        }, assertTimeout);
+  });
 
 
-        setInterval(function() {
-            boss.publish('expensive', null, {singletonSeconds: singletonSeconds})
-                .then(function() { publishCount++; });
-        }, publishInterval);
+  it('should queue successfully into next time slot if throttled', function (finished) {
 
-    });
+    this.timeout(3000);
 
+    const jobName = 'singletonPerDayWithFriends';
 
-    it('should queue successfully into next time slot if throttled', function (finished) {
+    boss.publish(jobName, null, {singletonHours: 1})
+      .then(jobId => {
+        assert.isOk(jobId);
+        return boss.publish(jobName, null, {singletonHours: 1, singletonNextSlot:true});
+      })
+      .then(jobId => {
+        assert.isOk(jobId);
+        finished();
+      });
 
-        this.timeout(3000);
-
-        const jobName = 'singletonPerDayWithFriends';
-
-        boss.publish(jobName, null, {singletonHours: 1})
-            .then(jobId => {
-                assert.isOk(jobId);
-                return boss.publish(jobName, null, {singletonHours: 1, singletonNextSlot:true});
-            })
-            .then(jobId => {
-                assert.isOk(jobId);
-                finished();
-            });
-
-    });
+  });
 
 
-    it('should reject 2nd request in the same time slot', function (finished) {
+  it('should reject 2nd request in the same time slot', function (finished) {
 
-        this.timeout(3000);
+    this.timeout(3000);
 
-        const jobName = 'singletonPerDay';
+    const jobName = 'singletonPerDay';
 
-        boss.publish(jobName, null, {singletonDays: 1})
-            .then(jobId => {
-                assert.isOk(jobId);
-                return boss.publish(jobName, null, {singletonDays: 1});
-            })
-            .then(jobId => {
-                assert.isNotOk(jobId);
-                finished();
-            });
+    boss.publish(jobName, null, {singletonDays: 1})
+      .then(jobId => {
+        assert.isOk(jobId);
+        return boss.publish(jobName, null, {singletonDays: 1});
+      })
+      .then(jobId => {
+        assert.isNotOk(jobId);
+        finished();
+      });
 
-    });
+  });
 
 });
