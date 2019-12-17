ce577566ca70b185e49f57870b4bd6d6861feff7
Tim Jones <me.timjones@gmail.com>
2019-12-17 16:19:26 -0600

    flattening bootstrapping queries to prevent deadlocks

--- src/boss.js
+++ src/boss.js
@@ -2,76 +2,93 @@ const EventEmitter = require('events');
 const plans = require('./plans');
 
 const events = {
-  error: 'error',
-  archived: 'archived',
-  deleted: 'deleted',
-  expired: 'expired',
-  monitorStates: 'monitor-states'
-};
+    error: 'error',
+    archived: 'archived',
+    deleted: 'deleted',
+    expired: 'expired',
+    monitorStates: 'monitor-states'
+}
 
-class Boss extends EventEmitter{
-  constructor(db, config){
-    super();
+class Boss extends EventEmitter {
+    constructor(db, config) {
+        super();
 
-    this.db = db;
-    this.config = config;
+        this.db = db;
+        this.config = config;
 
-    this.timers = {};
-    this.events = events;
+        this.timers = {};
+        this.events = events;
 
-    this.expireCommand = plans.expire(config.schema);
-    this.archiveCommand = plans.archive(config.schema);
-    this.purgeCommand = plans.purge(config.schema);
-    this.countStatesCommand = plans.countStates(config.schema);
-  }
-
-  supervise(){
-    const self = this;
-
-    // todo: add query that calcs avg start time delta vs. creation time
-
-    return Promise.all([
-      monitor(this.archive, this.config.archiveCheckInterval),
-      monitor(this.purge, this.config.deleteCheckInterval),
-      monitor(this.expire, this.config.expireCheckInterval),
-      this.config.monitorStateInterval ? monitor(this.countStates, this.config.monitorStateInterval) : null
-    ]);
-
-    function monitor(func, interval) {
-
-      return exec().then(repeat);
-
-      function exec() {
-        return func.call(self).catch(error => self.emit(events.error, error));
-      }
-
-      function repeat(){
-        if(self.stopped) return;
-        self.timers[func.name] = setTimeout(() => exec().then(repeat), interval);
-      }
+        this.expireCommand = plans.expire(config.schema);
+        this.archiveCommand = plans.archive(config.schema);
+        this.purgeCommand = plans.purge(config.schema);
+        this.countStatesCommand = plans.countStates(config.schema);
     }
-  }
 
-  stop() {
-    if(this.stopped) return Promise.resolve();
+    async supervise() {
+        const self = this;
 
-    this.stopped = true;
-    Object.keys(this.timers).forEach(key => clearTimeout(this.timers[key]));
-    
-    return Promise.resolve();
-  }
+        await monitor(() => this.archive(), this.config.archiveCheckInterval)
+        await monitor(() => this.purge(), this.config.deleteCheckInterval)
+        await monitor(() => this.expire(), this.config.expireCheckInterval)
 
-  countStates(){
-    let stateCountDefault = Object.assign({}, plans.states);
+        if(this.config.monitorStateInterval) {
+            await monitor(() => this.countStates(), this.config.monitorStateInterval)
+        }
 
-    Object.keys(stateCountDefault).forEach(key => stateCountDefault[key] = 0);
+        async function monitor(func, interval) {
 
-    return this.db.executeSql(this.countStatesCommand)
-      .then(counts => {
+            await exec()
+            repeat()
 
-        let states = counts.rows.reduce((acc, item) => {
-            if(item.name)
-              acc.queues[item.name] = acc.queues[item.name] || Object.assign({}, stateCountDefault);
+            async function exec() {
+                if(self.stopped) {
+                    return
+                }
+
+                try {
+                    return await func.call(self)
+                } catch(err) {
+                    self.emit(events.error, err)
+                }
+            }
+
+            function repeat(){
+                if(self.stopped) {
+                    return
+                }
+
+                self.timers[func.name] = setTimeout(function timer() {
+                    exec().then(repeat)
+                }, interval)
+            }
+        }
+    }
+
+    async stop() {
+        if(this.stopped) return;
+
+        this.stopped = true;
+
+        Object.keys(this.timers).forEach(key => clearTimeout(this.timers[key]));
+    }
+
+    async countStates() {
+
+        const stateCountDefault = Object.assign({}, plans.states);
+
+        Object.keys(stateCountDefault)
+              .forEach(key => stateCountDefault[key] = 0)
+
+        let defaultAcc = Object.assign({}, stateCountDefault, { queues: {} })
+
+        const counts = await this.db.executeSql(this.countStatesCommand)
+
+        const states = counts.rows.reduce((acc, item) => {
+            
+            if(item.name) {
+                acc.queues[item.name] = acc.queues[item.name] || Object.assign({}, stateCountDefault);
+            }
 
             let queue = item.name ? acc.queues[item.name] : acc;
             let state = item.state || 'all';
@@ -80,39 +97,38 @@ class Boss extends EventEmitter{
             queue[state] = parseFloat(item.size);
 
             return acc;
-          },
-          Object.assign({}, stateCountDefault, { queues: {} })
-        );
 
-        this.emit(events.monitorStates, states);
+        }, defaultAcc)
+        
 
-        return states;
-      });
-  }
+        this.emit(events.monitorStates, states)
 
-  expire() {
-    return this.db.executeSql(this.expireCommand)
-      .then(result => {
-        if (result.rowCount)
-          this.emit(events.expired, result.rowCount);
-      });
-  }
+        return states
+    }
 
-  archive(){
-    return this.db.executeSql(this.archiveCommand, [this.config.archiveCompletedJobsEvery])
-      .then(result => {
-        if (result.rowCount)
-          this.emit(events.archived, result.rowCount);
-      });
-  }
+    async expire() {
+        const result = await this.db.executeSql(this.expireCommand)
+        
+        if (result.rowCount) {
+            this.emit(events.expired, result.rowCount)
+        }
+    }
 
-  purge(){
-    return this.db.executeSql(this.purgeCommand, [this.config.deleteArchivedJobsEvery])
-      .then(result => {
-        if (result.rowCount)
-          this.emit(events.deleted, result.rowCount);
-      });
-  }
+    async archive(){
+        const result = await this.db.executeSql(this.archiveCommand, [this.config.archiveCompletedJobsEvery])
+        
+        if (result.rowCount) {
+            this.emit(events.archived, result.rowCount)
+        }
+    }
+
+    async purge(){
+        const result = await this.db.executeSql(this.purgeCommand, [this.config.deleteArchivedJobsEvery])
+        
+        if (result.rowCount) {
+            this.emit(events.deleted, result.rowCount)
+        }
+    }
 
 }
 
