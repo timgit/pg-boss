52246196c2f9d9ca920ec2be24cd7f2c2f1fd5d8
Tim Jones <me.timjones@gmail.com>
2019-12-18 09:16:53 -0600

    async await db tests

--- test/databaseTest.js
+++ test/databaseTest.js
@@ -5,146 +5,116 @@ const helper = require('./testHelper');
 
 describe('database', function(){
 
-  this.timeout(15000);
+    this.timeout(10000);
 
-  it('should fail on invalid database host', function(finished){
+    it('should fail on invalid database host', async function() {
 
-    const boss = new PgBoss('postgres://bobby:tables@wat:12345/northwind');
+        const boss = new PgBoss('postgres://bobby:tables@wat:12345/northwind');
 
-    boss.start()
-      .then(() => {
-        assert(false);
-        return boss.stop();
-      })
-      .then(() => finished())
-      .catch(() => finished());
-  });
+        try {
+            await boss.start()
+            assert(false)
+        } catch {
+            await boss.stop()
+        }
 
+    })
 
-  it('should work with a custom schema name', function(finished){
+    it('should work with a custom schema name', async function() {
 
-    const jobName = 'custom-schema';
+        const jobName = 'custom-schema';
 
-    let config = helper.getConfig();
-    config.schema = '_queue';
+        let config = helper.getConfig();
+        config.schema = '_queue';
 
-    let boss, jobId;
+        try {
+            const boss = await helper.start(config)
+            const jobId = await boss.publish(jobName)
+            const job = await boss.fetch(jobName)
 
-    helper.start(config)
-      .then(result => boss = result)
-      .then(() => boss.publish(jobName))
-      .then(id => jobId = id)
-      .then(() => boss.fetch(jobName))
-      .then(job => {
-        assert(job.id === jobId);
-        finished();
-      })
-      .catch(error => {
-          assert(false, error.message);
-          finished();
-      });
+            assert(job.id === jobId)
+        } catch(err) {
+            assert(false, err.message)
+        }
 
-  });
+    });
 
-  it('can be swapped out via BYODB', function(finished) {
+    it('can be swapped out via BYODB', async function() {
 
-    const query = 'SELECT something FROM somewhere';
+        const query = 'SELECT something FROM somewhere'
 
-    const mydb = {
-      executeSql: (text, values) => Promise.resolve({rows:[], text, rowCount: 0})
-    };
+        const mydb = {
+            executeSql: async (text, values) => ({ rows:[], text, rowCount: 0 })
+        }
 
-    const boss = new PgBoss({db:mydb});
+        const boss = new PgBoss({ db: mydb })
 
-    boss.start()
-      .then(() => boss.db.executeSql(query))
-      .then(response => assert(response.text === query))
-      .then(() => boss.stop())
-      .then(() => finished())
-      .catch(err => {
-        console.error(err.message);
-        finished();
-      });
+        await boss.start()
 
-  });
+        const response = await boss.db.executeSql(query)
+        
+        assert(response.text === query)
+        
+        await boss.stop()
+        
+    })
 
+    it('connection count does not exceed configured pool size with `poolSize`', async function() {
 
-  it('connection count does not exceed configured pool size with `poolSize`', function(finished){
+        const listenerCount = 100;
+        const poolSize = 5;
+        const configOption = 'poolSize'
 
-    const listenerCount = 100;
-    const poolSize = 5;
+        await poolSizeConnectionTest(listenerCount, poolSize, configOption)
 
-    let listeners = [];
-    for(let x = 0; x<listenerCount; x++)
-      listeners[x] = x;
+    });
 
-    let boss;
-    let prevConnectionCount;
+    it('connection count does not exceed configured pool size with `max`', async function() {
 
-    helper.start({poolSize})
-      .then(b => boss = b)
-      .then(() => boss.db)
-      .then(() => countConnections(boss.db))
-      .then(connectionCount => prevConnectionCount = connectionCount)
-      .then(() => Promise.map(listeners, (val, index) => boss.subscribe(`job${index}`, () => {})))
-      .then(() => Promise.delay(3000))
-      .then(() => countConnections(boss.db))
-      .then(connectionCount => {
+        const listenerCount = 100;
+        const poolSize = 5;
+        const configOption = 'max'
+
+        await poolSizeConnectionTest(listenerCount, poolSize, configOption)
+
+    });
+
+    async function poolSizeConnectionTest(listenerCount, poolSize, configOption) {
+        
+        let listeners = [];
+        
+        for(let x = 0; x<listenerCount; x++) {
+            listeners[x] = x;
+        }
+
+        const boss = await helper.start({[configOption]: poolSize })      
+        const prevConnectionCount = await countConnections(boss.db)
+
+        await Promise.map(listeners, (val, index) => boss.subscribe(`job${index}`, () => {}))
+
+        await Promise.delay(3000)
+
+        const connectionCount = await countConnections(boss.db)
+
         let newConnections = connectionCount - prevConnectionCount;
+
         console.log(`listeners: ${listenerCount}  pool size: ${poolSize}`);
         console.log('connections:');
         console.log(`  before subscribing: ${prevConnectionCount}  now: ${connectionCount}  new: ${newConnections}`);
-        assert(newConnections <= poolSize);
-      })
-      .then(() => boss.stop())
-      .then(() => finished());
 
+        assert(newConnections <= poolSize)
 
-    function countConnections(db) {
-      return db.executeSql('SELECT count(*) as connections FROM pg_stat_activity WHERE application_name=$1',
-        [boss.db.config.application_name])
-        .then(result => parseFloat(result.rows[0].connections));
+        await boss.stop()
+
+        async function countConnections(db) {
+            
+            const sql = 'SELECT count(*) as connections FROM pg_stat_activity WHERE application_name=$1'
+            const values = [ boss.db.config.application_name ]
+
+            const result = await db.executeSql(sql, values)
+
+            return parseFloat(result.rows[0].connections)
+        }
     }
-
-  });
-
-  it('connection count does not exceed configured pool size with `max`', function(finished){
-
-    const listenerCount = 100;
-    const max = 5;
-
-    let listeners = [];
-    for(let x = 0; x<listenerCount; x++)
-      listeners[x] = x;
-
-    let boss;
-    let prevConnectionCount;
-
-    helper.start({max})
-      .then(b => boss = b)
-      .then(() => boss.db)
-      .then(() => countConnections(boss.db))
-      .then(connectionCount => prevConnectionCount = connectionCount)
-      .then(() => Promise.map(listeners, (val, index) => boss.subscribe(`job${index}`, () => {})))
-      .then(() => Promise.delay(3000))
-      .then(() => countConnections(boss.db))
-      .then(connectionCount => {
-        let newConnections = connectionCount - prevConnectionCount;
-        console.log(`listeners: ${listenerCount}  pool size: ${max}`);
-        console.log('connections:');
-        console.log(`  before subscribing: ${prevConnectionCount}  now: ${connectionCount}  new: ${newConnections}`);
-        assert(newConnections <= max);
-      })
-      .then(() => boss.stop())
-      .then(() => finished());
-
-
-    function countConnections(db) {
-      return db.executeSql('SELECT count(*) as connections FROM pg_stat_activity WHERE application_name=$1',
-        [boss.db.config.application_name])
-        .then(result => parseFloat(result.rows[0].connections));
-    }
-
-  });
 
 });
