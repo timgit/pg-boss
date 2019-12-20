e55a02f4b2eba512fe3459772c4b5925b75b0773
Tim Jones <me.timjones@gmail.com>
2019-12-20 17:11:28 -0600

    coverage cases

--- test/opsTests.js
+++ test/opsTests.js
@@ -10,18 +10,26 @@ describe('ops', function () {
     await boss.stop()
   })
 
+  it('should start back up after stopping', async function () {
+    await boss.start()
+    await boss.stop()
+  })
+
   it('should expire manually', async function () {
     await boss.connect()
     await boss.expire()
+    await boss.disconnect()
   })
 
   it('should archive manually', async function () {
     await boss.connect()
     await boss.archive()
+    await boss.disconnect()
   })
 
   it('should purge the archive manually', async function () {
     await boss.connect()
     await boss.purge()
+    await boss.disconnect()
   })
 })
