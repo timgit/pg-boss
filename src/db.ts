c45bb4cd4e350d67cdd9b9dbfc82b9a95f0f71aa
Roman Filippov <rf@romanfilippov.com>
2020-03-19 21:34:29 +0700

    refactor: converts db to TypeScript

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,12 @@
-const EventEmitter = require('events')
-const pg = require('pg')
+import { EventEmitter } from 'events'
+import { Pool, PoolConfig } from 'pg'
+
+interface DbConfig extends PoolConfig {
+  poolSize: number
+}
 
 class Db extends EventEmitter {
-  constructor (config) {
+  constructor(private config: DbConfig) {
     super()
 
     if (config.poolSize) {
@@ -10,26 +14,31 @@ class Db extends EventEmitter {
     }
 
     config.application_name = config.application_name || 'pgboss'
-
-    this.config = config
   }
 
+  private pool: Pool
+
+  public opened = false
+
   async open () {
-    this.pool = new pg.Pool(this.config)
+    this.pool = new Pool(this.config)
     this.pool.on('error', error => this.emit('error', error))
     this.opened = true
   }
 
   async close () {
+    // TODO: fix, using undocumented "ending" property
+    // @ts-ignore
     if (!this.pool.ending) {
       await this.pool.end()
       this.opened = false
     }
   }
 
-  async executeSql (text, values) {
+  async executeSql(text: Parameters<Pool['query']>[0], values?: Parameters<Pool['query']>[1]) {
     return this.pool.query(text, values)
   }
 }
 
-module.exports = Db
+// TODO: export class directly when tests are rewritten & disable esModuleInterop
+export = Db
