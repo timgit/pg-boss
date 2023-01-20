ecd770b87860506cadd845455f74cd61134d4ac2
ismael-iskauskas <47241324+ismael-iskauskas@users.noreply.github.com>
2023-01-20 01:12:00 +0100

    added test for after-purge event

--- test/deleteTest.js
+++ test/deleteTest.js
@@ -4,7 +4,8 @@ const delay = require('delay')
 
 describe('delete', async function () {
   const defaults = {
-    deleteAfterSeconds: 1,
+    deleteAfterSeconds: 2,
+    archiveCompletedAfterSeconds: 1,
     maintenanceIntervalSeconds: 1
   }
 
@@ -17,13 +18,33 @@ describe('delete', async function () {
     const job = await boss.fetch(jobName)
 
     assert.strictEqual(jobId, job.id)
-
     await boss.complete(jobId)
-
     await delay(7000)
 
     const archivedJob = await helper.getArchivedJobById(config.schema, jobId)
 
     assert.strictEqual(archivedJob, null)
+  })
+
+  it('should delete an archived job and trigger after-purge', async function () {
+    const jobName = 'deleteMe'
+
+    const config = { ...this.test.bossConfig, ...defaults }
+    const boss = this.test.boss = await helper.start(config)
+    const jobId = await boss.send(jobName)
+    const job = await boss.fetch(jobName)
+    const purgePromise = new Promise(resolve => this.test.boss.once('after-purge', (jobIds) => {
+      return resolve(jobIds)
+    }))
+
+    assert.strictEqual(jobId, job.id)
+    await boss.complete(jobId)
+    await delay(7000)
+
+    const archivedJob = await helper.getArchivedJobById(config.schema, jobId)
+
+    assert.strictEqual(archivedJob, null)
+    const deletedJobIds = await purgePromise
+    assert.strictEqual(deletedJobIds.includes(jobId), true)
   })
 })
