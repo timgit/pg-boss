a15aaaa602d877bc914411422778761dc3324976
Tim Jones <me.timjones@gmail.com>
2023-02-02 22:35:50 -0600

    added readme convenience script

---
+++ test/readme.js
@@ -0,0 +1,26 @@
+const helper = require('./testHelper')
+
+async function readme () {
+  const PgBoss = require('../src')
+  const boss = new PgBoss(helper.getConnectionString())
+
+  boss.on('error', error => {
+    console.error(error)
+  })
+
+  await boss.start()
+
+  const queue = 'some-queue'
+
+  await boss.schedule(queue, '* * * * *')
+
+  console.log(`created cronjob in queue ${queue}`)
+
+  await boss.work(queue, someAsyncJobHandler)
+}
+
+async function someAsyncJobHandler (job) {
+  console.log(`running job ${job.id}`)
+}
+
+readme()
