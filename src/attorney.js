56ee195bb35c77fc447c6862a3c51cfd88e926b1
Matt Casey <matt@mattcasey.nyc>
2024-07-12 19:57:18 -0400

    Fix regex to check for digits instead of the character "d"

--- src/attorney.js
+++ src/attorney.js
@@ -179,7 +179,7 @@ function assertPostgresObjectName (name) {
   assert(typeof name === 'string', 'Name must be a string')
   assert(name.length <= 50, 'Name cannot exceed 50 characters')
   assert(!/\W/.test(name), 'Name can only contain alphanumeric characters and underscores')
-  assert(!/^d/.test(name), 'Name cannot start with a number')
+  assert(!/^\d/.test(name), 'Name cannot start with a number')
 }
 
 function applyArchiveConfig (config) {
