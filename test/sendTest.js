3d9508fc0134789853c82078bd62c77fb2ea020c
Tim Jones <me.timjones@gmail.com>
2025-10-07 09:09:20 -0500

    add missing retention arg for send

--- test/sendTest.js
+++ test/sendTest.js
@@ -136,4 +136,43 @@ describe('send', function () {
 
     assert(!job)
   })
+
+  it('should create job with all properties', async function () {
+    const boss = this.test.boss = await helper.start(this.test.bossConfig)
+    const queue = this.test.bossConfig.schema
+
+    const deadLetter = `${queue}_dlq`
+    await boss.createQueue(deadLetter)
+    await boss.updateQueue(queue, { deadLetter })
+
+    const options = {
+      priority: 1,
+      retryLimit: 1,
+      retryDelay: 2,
+      retryBackoff: true,
+      retryDelayMax: 3,
+      startAfter: new Date().toISOString(),
+      expireInSeconds: 5,
+      deleteAfterSeconds: 60,
+      singletonKey: '123',
+      retentionSeconds: 10
+    }
+
+    const keepUntil = new Date(new Date(options.startAfter).getTime() + (options.retentionSeconds * 1000)).toISOString()
+
+    const id = await boss.send(queue, null, options)
+
+    const job = await boss.getJobById(queue, id)
+
+    assert.strictEqual(job.priority, options.priority, `priority input ${options.priority} didn't match job ${job.priority}`)
+    assert.strictEqual(job.retryLimit, options.retryLimit, `retryLimit input ${options.retryLimit} didn't match job ${job.retryLimit}`)
+    assert.strictEqual(job.retryDelay, options.retryDelay, `retryDelay input ${options.retryDelay} didn't match job ${job.retryDelay}`)
+    assert.strictEqual(job.retryBackoff, options.retryBackoff, `retryBackoff input ${options.retryBackoff} didn't match job ${job.retryBackoff}`)
+    assert.strictEqual(job.retryDelayMax, options.retryDelayMax, `retryDelayMax input ${options.retryDelayMax} didn't match job ${job.retryDelayMax}`)
+    assert.strictEqual(new Date(job.startAfter).toISOString(), options.startAfter, `startAfter input ${options.startAfter} didn't match job ${job.startAfter}`)
+    assert.strictEqual(job.expireInSeconds, options.expireInSeconds, `expireInSeconds input ${options.expireInSeconds} didn't match job ${job.expireInSeconds}`)
+    assert.strictEqual(job.deleteAfterSeconds, options.deleteAfterSeconds, `deleteAfterSeconds input ${options.deleteAfterSeconds} didn't match job ${job.deleteAfterSeconds}`)
+    assert.strictEqual(job.singletonKey, options.singletonKey, `name input ${options.singletonKey} didn't match job ${job.singletonKey}`)
+    assert.strictEqual(new Date(job.keepUntil).toISOString(), keepUntil, `keepUntil input ${keepUntil} didn't match job ${job.keepUntil}`)
+  })
 })
