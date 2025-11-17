58b9f08cd99cb5d3d4ded045d12915e341705308
Matt Sergeant <helpme@gmail.com>
2025-11-17 17:15:00 -0500

    Support both cjs and esm modules simultaneously from npm

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['./src/index.ts'],
-  format: 'es',
+  format: ['es', 'cjs'],
   dts: true,
-  exports: true
+  outExtensions: ({ format }) => {
+    if (format === 'cjs') {
+      return { js: '.cjs', dts: '.d.cts' }
+    }
+
+    return { js: '.mjs', dts: '.d.ts' }
+  }
 })
