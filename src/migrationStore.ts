e03ec820f438e9e4e62582bd35274248dcc87db8
Tim Jones <me.timjones@gmail.com>
2026-01-25 15:11:20 -0600

    migration updates

--- src/migrationStore.ts
+++ src/migrationStore.ts
@@ -250,7 +250,6 @@ function getAll (schema: string): types.Migration[] {
         `,
         `ALTER TABLE ${schema}.job ADD COLUMN IF NOT EXISTS group_id text`,
         `ALTER TABLE ${schema}.job ADD COLUMN IF NOT EXISTS group_tier text`,
-        // Update create_queue function to include the group concurrency index for partitioned tables
         `
         CREATE OR REPLACE FUNCTION ${schema}.create_queue(queue_name text, options jsonb)
         RETURNS VOID AS
@@ -347,20 +346,97 @@ function getAll (schema: string): types.Migration[] {
         )`
       ],
       uninstall: [
-        `ALTER INDEX IF EXISTS ${schema}.job_common_i6 RENAME TO job_i6`,
-        `ALTER INDEX IF EXISTS ${schema}.job_common_i5 RENAME TO job_i5`,
-        `ALTER INDEX IF EXISTS ${schema}.job_common_i4 RENAME TO job_i4`,
-        `ALTER INDEX IF EXISTS ${schema}.job_common_i3 RENAME TO job_i3`,
-        `ALTER INDEX IF EXISTS ${schema}.job_common_i2 RENAME TO job_i2`,
-        `ALTER INDEX IF EXISTS ${schema}.job_common_i1 RENAME TO job_i1`,
-        `SELECT ${schema}.job_table_run('DROP INDEX IF EXISTS ${schema}.job_i7')`,
-        `DROP FUNCTION IF EXISTS ${schema}.job_table_run(text)`,
-        `DROP FUNCTION IF EXISTS ${schema}.job_table_run_async(text, int, text)`,
-        `DROP FUNCTION IF EXISTS ${schema}.job_table_format(text, text)`,
-        `DROP TABLE IF EXISTS ${schema}.bam`,
-        `ALTER TABLE ${schema}.version DROP COLUMN IF EXISTS bam_on`,
-        `ALTER TABLE ${schema}.job DROP COLUMN IF EXISTS group_tier`,
-        `ALTER TABLE ${schema}.job DROP COLUMN IF EXISTS group_id`
+        `ALTER INDEX ${schema}.job_common_i6 RENAME TO job_i6`,
+        `ALTER INDEX ${schema}.job_common_i5 RENAME TO job_i5`,
+        `ALTER INDEX ${schema}.job_common_i4 RENAME TO job_i4`,
+        `ALTER INDEX ${schema}.job_common_i3 RENAME TO job_i3`,
+        `ALTER INDEX ${schema}.job_common_i2 RENAME TO job_i2`,
+        `ALTER INDEX ${schema}.job_common_i1 RENAME TO job_i1`,
+        `SELECT ${schema}.job_table_run('DROP INDEX ${schema}.job_i7')`,
+        `
+        CREATE OR REPLACE FUNCTION ${schema}.create_queue(queue_name text, options jsonb)
+        RETURNS VOID AS
+        $$
+        DECLARE
+          tablename varchar := CASE WHEN options->>'partition' = 'true'
+                                THEN 'j' || encode(sha224(queue_name::bytea), 'hex')
+                                ELSE 'job_common'
+                                END;
+          queue_created_on timestamptz;
+        BEGIN
+
+          WITH q as (
+            INSERT INTO ${schema}.queue (
+              name,
+              policy,
+              retry_limit,
+              retry_delay,
+              retry_backoff,
+              retry_delay_max,
+              expire_seconds,
+              retention_seconds,
+              deletion_seconds,
+              warning_queued,
+              dead_letter,
+              partition,
+              table_name
+            )
+            VALUES (
+              queue_name,
+              options->>'policy',
+              COALESCE((options->>'retryLimit')::int, 2),
+              COALESCE((options->>'retryDelay')::int, 0),
+              COALESCE((options->>'retryBackoff')::bool, false),
+              (options->>'retryDelayMax')::int,
+              COALESCE((options->>'expireInSeconds')::int, 900),
+              COALESCE((options->>'retentionSeconds')::int, 1209600),
+              COALESCE((options->>'deleteAfterSeconds')::int, 604800),
+              COALESCE((options->>'warningQueueSize')::int, 0),
+              options->>'deadLetter',
+              COALESCE((options->>'partition')::bool, false),
+              tablename
+            )
+            ON CONFLICT DO NOTHING
+            RETURNING created_on
+          )
+          SELECT created_on into queue_created_on from q;
+
+          IF queue_created_on IS NULL OR options->>'partition' IS DISTINCT FROM 'true' THEN
+            RETURN;
+          END IF;
+
+          EXECUTE format('CREATE TABLE ${schema}.%I (LIKE ${schema}.job INCLUDING DEFAULTS)', tablename);
+
+          EXECUTE format('ALTER TABLE ${schema}.%1$I ADD PRIMARY KEY (name, id)', tablename);
+          EXECUTE format('ALTER TABLE ${schema}.%1$I ADD CONSTRAINT q_fkey FOREIGN KEY (name) REFERENCES ${schema}.queue (name) ON DELETE RESTRICT DEFERRABLE INITIALLY DEFERRED', tablename);
+          EXECUTE format('ALTER TABLE ${schema}.%1$I ADD CONSTRAINT dlq_fkey FOREIGN KEY (dead_letter) REFERENCES ${schema}.queue (name) ON DELETE RESTRICT DEFERRABLE INITIALLY DEFERRED', tablename);
+
+          EXECUTE format('CREATE INDEX %1$s_i5 ON ${schema}.%1$I (name, start_after) INCLUDE (priority, created_on, id) WHERE state < ''active''', tablename);
+          EXECUTE format('CREATE UNIQUE INDEX %1$s_i4 ON ${schema}.%1$I (name, singleton_on, COALESCE(singleton_key, '''')) WHERE state <> ''cancelled'' AND singleton_on IS NOT NULL', tablename);
+
+          IF options->>'policy' = 'short' THEN
+            EXECUTE format('CREATE UNIQUE INDEX %1$s_i1 ON ${schema}.%1$I (name, COALESCE(singleton_key, '''')) WHERE state = ''created'' AND policy = ''short''', tablename);
+          ELSIF options->>'policy' = 'singleton' THEN
+            EXECUTE format('CREATE UNIQUE INDEX %1$s_i2 ON ${schema}.%1$I (name, COALESCE(singleton_key, '''')) WHERE state = ''active'' AND policy = ''singleton''', tablename);
+          ELSIF options->>'policy' = 'stately' THEN
+            EXECUTE format('CREATE UNIQUE INDEX %1$s_i3 ON ${schema}.%1$I (name, state, COALESCE(singleton_key, '''')) WHERE state <= ''active'' AND policy = ''stately''', tablename);
+          ELSIF options->>'policy' = 'exclusive' THEN
+            EXECUTE format('CREATE UNIQUE INDEX %1$s_i6 ON ${schema}.%1$I (name, COALESCE(singleton_key, '''')) WHERE state <= ''active'' AND policy = ''exclusive''', tablename);
+          END IF;
+
+          EXECUTE format('ALTER TABLE ${schema}.%I ADD CONSTRAINT cjc CHECK (name=%L)', tablename, queue_name);
+          EXECUTE format('ALTER TABLE ${schema}.job ATTACH PARTITION ${schema}.%I FOR VALUES IN (%L)', tablename, queue_name);
+        END;
+        $$
+        LANGUAGE plpgsql;
+        `,
+        `DROP FUNCTION ${schema}.job_table_run(text, text, text)`,
+        `DROP FUNCTION ${schema}.job_table_run_async(text, int, text, text, text)`,
+        `DROP FUNCTION ${schema}.job_table_format(text, text)`,
+        `DROP TABLE ${schema}.bam`,
+        `ALTER TABLE ${schema}.version DROP COLUMN bam_on`,
+        `ALTER TABLE ${schema}.job DROP COLUMN group_tier`,
+        `ALTER TABLE ${schema}.job DROP COLUMN group_id`
       ]
     }
   ]
